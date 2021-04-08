9304c905a7e72ba1f61a450d20669b4984da9f6b
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-04-08 15:52:00 -0400

    updating

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function renderCafe(doc){
     let name = document.createElement('span')
     let city = document.createElement('span')
     let cross = document.createElement('div')
+    let btn = document.createElement('button')
 
     //the doc.id is the auto-generated id from the document in a collection       
     li.setAttribute('data-id', doc.id)
@@ -15,11 +16,17 @@ function renderCafe(doc){
     name.textContent = doc.data().name
     city.textContent = doc.data().city
     cross.textContent = 'X'
+    btn.textContent = 'Update'
+    btn.className = "update-btn"
+
+    name.contentEditable = true
+    city.contentEditable = true
 
     //append the element onto the DOM
     li.appendChild(name)
     li.appendChild(city)
     li.appendChild(cross)
+    li.appendChild(btn)
 
     cafeList.appendChild(li)
 
@@ -33,6 +40,19 @@ function renderCafe(doc){
         //use the delete() to remove it from the database
         db.collection('cafes').doc(id).delete()
     })
+
+      //UPDATING DATA
+      btn.addEventListener('click', (evt)=>{
+        let id = evt.target.parentElement.getAttribute('data-id')
+        let name = evt.target.parentElement.children[0].textContent
+        let city = evt.target.parentElement.children[1].textContent 
+        db.collection('cafes').doc(id).update({
+            name: name,
+            city: city
+        })
+    })
+
+    // updating(btn)
 }
 
 // GETTING DATA
@@ -61,9 +81,12 @@ function renderCafe(doc){
 
 // Real Time Listener GETTING DATA
 //onSnapshot is a eventListener
+//you can use onSnapshot() instead of get() to listen to the results of a query.
+//snapshot handler will receive a new query snapshot every time the query results change
 db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    //docChanges() allows actual changes to query results between query snapshots
+    //there will be a property called type that will have added or removed or modified
     let changes = snapshot.docChanges()
-    // there will be a property called type that will have added or removed
     // console.log(changes)
     changes.forEach(change => {
         // console.log(change.doc.data())
@@ -91,4 +114,28 @@ form.addEventListener('submit', (evt) => {
     form.city.value = ''
 })
 
+
+
+
+// UPDATING DATA
+// db.collection('cafes').doc('TH3Eh7rWDWl6Hzt0VsbW').update({city: 'new world'})
+
+function updating(btn){
+    btn.addEventListener('click', (evt)=>{
+        let id = evt.target.parentElement.getAttribute('data-id')
+        let name = evt.target.parentElement.children[0].textContent
+        let city = evt.target.parentElement.children[1].textContent 
+        db.collection('cafes').doc(id).update({
+            name: name,
+            city: city
+        })
+    })
+}
+
+// USING .set() instead of .update()
+// set() overwrite all the property. Disregard old properties.
+// if you have 4 property. when you use .set({one: somevalue}). now you will only have that one new property.
+// if you have 4 property. when you use .update({one: somevalue}). now you will still have four property.
+// db.collection('cafes').doc('TH3Eh7rWDWl6Hzt0VsbW').set({city: 'new world'})
+// no longer will have the name property. Will only have the new city property
 
